77e2b6972d967551bf995c7328a7ac364ada047b
wqd190004-rishi <wqd190004@siswa.um.edu.my>
2024-04-08 17:27:54 +0100

    adding higher order function info

---
+++ python_learning_revision/higher_order_fn.py
@@ -0,0 +1,24 @@
+# writing function as an argument called higher order function
+def hello(name):
+	return name + '!!'
+
+def who(func, val):
+	return func(val)
+
+hi = who(hello,'R')
+#print(hi)
+
+## map ##
+
+## using map(function, iterable)
+int_lst = range(1,11)
+def double(x):
+	return x*2
+
+# doubled = map(double, int_lst)
+# print(list(doubled))
+
+# another way to write
+doubled = map(lambda x: x*2, int_lst)
+#print(list(doubled))
+
